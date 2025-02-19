e927f125c47a604d6d3a5cb58772909c2272067f
isidore9248 <2279390498@qq.com>
2025-02-19 15:32:18 +0800

    增加c，c++混合编程模板

---
+++ c,c++混合编程/new_c.c
@@ -0,0 +1,8 @@
+#include "new_c.h"
+
+#include <stdio.h>
+
+void new_c_init()
+{
+	printf("it is in new_c init\n");
+}
---
+++ c,c++混合编程/new_c.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif //
+
+	void new_c_init();
+
+#ifdef __cplusplus
+}
+#endif
